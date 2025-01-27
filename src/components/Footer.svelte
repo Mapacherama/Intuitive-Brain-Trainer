bf91eb47a6491f105a7d08bcd3f521017bfe769b
Mapacherama <48964493+Mapacherama@users.noreply.github.com>
2025-01-27 06:45:58 +0100

    Setting up the header and footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <p>Made with ❤️ using Vite + Svelte</p>
+  </footer>
+  
+  <style>
+    .footer {
+      background-color: var(--dark-mint);
+      color: var(--white);
+      text-align: center;
+      padding: 16px;
+    }
+  </style>
+  
